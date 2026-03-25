747416015bd20af4cc8c11fa78ff3911d3023fac
ht2850 <ht2850@srmist.edu.in>
2026-03-25 09:16:36 +0530

    UC1: Initialize Train and Display Consist Summary

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        System.out.println("======================================= ");
+        System.out.println("   === Train Consist Management App === ");
+        System.out.println("====== Train Consist Management App === \n");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial number of bogies: " + trainConsist.size());
+
     }
 }
